1910e1cc2fa5b0176551ebb1871474895acb3845
don4 <don4@users.noreply.github.com>
2022-09-20 20:44:20 +0530

    updated import and package.json

--- pages/test/index.test.jsx
+++ pages/test/index.test.jsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import Home from '../index';
+import Home from '../index.page';
 
 describe('Home', () => {
   it('renders a heading', () => {
